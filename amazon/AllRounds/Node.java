eecdd963eea711ce79d86044b9463d717ecfee02
sumitdanish <sumit.polite@gmail.com>
2015-04-06 16:37:43 +0530

    Create Node.java

---
+++ amazon/AllRounds/Node.java
@@ -0,0 +1,24 @@
+package amazon.AllRounds;
+
+public class Node {
+
+	private int data;
+	private Node next;
+	public Node(int data){
+		this.data = data;
+	}
+	public void setNext(Node next){
+		this.next = next;
+	}
+	public void setData(int data){
+		this.data = data;
+	}
+	public int getData(){
+		return data;
+	}
+	public Node getNext(){
+		return next;
+	}
+	
+	
+}
